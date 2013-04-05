7f68ea1a347477d761b7e203219846a7f703261f
gc <meguojian@163.com>
2013-04-05 17:45:45 +0800

    init

---
+++ src/main/java/me/cn/framework_project/App.java
@@ -0,0 +1,13 @@
+package me.cn.framework_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
